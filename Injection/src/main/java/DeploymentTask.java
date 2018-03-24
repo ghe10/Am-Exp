89a5eceae97444ca938e689eb784fced3e30312e
ghe10 <gche1993@gmail.com>
2018-03-24 17:51:31 -0500

    Add basic inject code, an Annotation example and a @Provides example

---
+++ Injection/src/main/java/DeploymentTask.java
@@ -0,0 +1,11 @@
+public class DeploymentTask implements Task{
+    private String name = "Empty task";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
