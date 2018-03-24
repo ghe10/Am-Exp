017a540b5d26c06a2bf54a620d43ead1d26cfdcd
ghe10 <gche1993@gmail.com>
2018-03-24 02:18:27 -0500

    Notes about the override func in class extend

---
+++ JavaNote.java
@@ -0,0 +1,5 @@
+/**
+ * when we have a new object, itself knows what class it is, it will call its overrided func even if we use
+ * Base obj = new ExtendBase(); or obj = (Base)obj;
+ * Casting child class to parent class is redundant!!!!!!!!
+ */
